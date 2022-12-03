6a41f85385dfd407b59588982d117a4e38aa3505
Abdulkarim Ogaji <abdulkarimogaji002@gmail.com>
2022-12-03 16:47:35 +0100

    fix

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-require('dotenv').config()
-const cors = require('cors')
+require("dotenv").config();
+const cors = require("cors");
 
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
 
-app.use(cors())
-
+app.use(cors());
 
 // Routes
-app.use('/', require('./routes/All'))
-app.use('/countries', require('./routes/countries'))
-app.use('/continent', require('./routes/continent'))
+app.use("/api", require("./routes/All"));
+app.use("/api/countries", require("./routes/countries"));
+app.use("/api/continent", require("./routes/continent"));
 
 //* Serve static assets in production, must be at this location of this file
 
-app.get('/', (req, res) => {
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')))
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "client", "dist")));
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
 
+const PORT = process.env.PORT || 5000;
 
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
